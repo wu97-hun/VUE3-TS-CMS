00221a8d711ffdda6ca1877177bb9011dc5ea47a
wujun <782353744@qq.com>
2022-05-05 16:15:29 +0800

    feat: 修改BASE_URL

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -3,11 +3,12 @@ const TIME_OUT = 10000
 
 if (process.env.NODE_ENV === 'development') {
   // 由于配置了devServer所以修改BSASE_URL这样才能映射
-  BASE_URL = '/api'
+  // BASE_URL = '/api'
+  BASE_URL = 'http://152.136.185.210:4000'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = '/api'
+  BASE_URL = 'http://152.136.185.210:4000'
 } else {
-  BASE_URL = '/api'
+  BASE_URL = 'http://152.136.185.210:4000'
 }
 
 export { BASE_URL, TIME_OUT }
