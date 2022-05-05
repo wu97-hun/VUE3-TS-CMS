cfcaf83580eab55574834b74083a4eec11a333e9
wujun <782353744@qq.com>
2022-05-05 16:01:01 +0800

    feat: 完结撒花！

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://152.136.185.210:5000',
+        target: 'http://152.136.185.210:4000',
         pathRewrite: {
           '^/api': ''
         },
